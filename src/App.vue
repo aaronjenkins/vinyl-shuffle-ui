6fbc75569673bf1029bdcc618e591e366abb72f0
aaronjenkins <aaron@aaronjenkins.me>
2021-11-26 13:00:11 -0500

    add api call

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <RandomVinyl />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RandomVinyl from './components/RandomVinyl.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    RandomVinyl
   }
 }
 </script>
---
+++ src/components/RandomVinyl.vue
@@ -0,0 +1,34 @@
+<template>
+    <p>{{dataReturned}}</p>
+</template>
+
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      dataReturned: [],
+      errors: []
+    };
+  },
+
+  async created() {
+    let url = `https://mndi3iv2cb.execute-api.us-east-2.amazonaws.com/default/vinylshuffle`;
+    let config = {
+      headers: {
+        "x-api-key": "5ZnRAbCUT59HeZgkWUxJL5OK0gA1elT59FEEeGPU"
+      }
+    };
+
+    try {
+      const response = await axios.get(url, config);
+      this.dataReturned = response
+    } catch (e) {
+      this.errors.push(e);
+    }
+  }
+  
+};
+</script>
